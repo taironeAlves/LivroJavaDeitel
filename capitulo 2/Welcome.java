0c23e47576e3a265c2342ce04d94c30897ecffc4
Estilotabi <115471921+taironeAlves@users.noreply.github.com>
2022-10-16 18:12:05 -0300

    Create Welcome.java

---
+++ capitulo 2/Welcome.java
@@ -0,0 +1,15 @@
+// programa que imprime texto
+
+public class Welcome 
+{
+	// Método main inicia a execução do aplicativo java
+	/**
+	 * @param args
+	 */
+	
+	public static void main(String[] args) {
+		System.out.println("Bem vindo ao curso de Java");
+	}	
+	
+
+}
