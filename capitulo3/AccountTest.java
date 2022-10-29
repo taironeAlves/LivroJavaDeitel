572138dfbd9d7eb2a61c267801c583623c8381d9
Estilotabi <115471921+taironeAlves@users.noreply.github.com>
2022-10-29 16:38:41 -0300

    update_291022

--- capitulo3/AccountTest.java
+++ capitulo3/AccountTest.java
@@ -1,23 +1,5 @@
 package capitulo3;
-// import java.util.Scanner;
 
-// public class AccountTest {
-//     public static void main(String[] args) {
-//         // cria um objeto do tipo Scanner
-//         Scanner input = new Scanner(System.in);
-
-//         //Cria um objeto do tipo account
-//         Account myAccount = new Account();
-
-//         //exibe o valor inical do nome()
-//         System.out.printf("Initial name is: s%n%n", myAccount.getName());
-
-//         //Solicita e 
-//     }
-
-// }
-// Fig. 3.2: AccountTest.java
-// Creating and manipulating an Account object.
 import java.util.Scanner;
 
 public class AccountTest
@@ -43,22 +25,4 @@ public class AccountTest
       System.out.printf("Name in object myAccount is:%n%s%n",
          myAccount.getName());
    } 
-} // end class AccountTest
-
-
-/**************************************************************************
- * (C) Copyright 1992-2014 by Deitel & Associates, Inc. and               *
- * Pearson Education, Inc. All Rights Reserved.                           *
- *                                                                        *
- * DISCLAIMER: The authors and publisher of this book have used their     *
- * best efforts in preparing the book. These efforts include the          *
- * development, research, and testing of the theories and programs        *
- * to determine their effectiveness. The authors and publisher make       *
- * no warranty of any kind, expressed or implied, with regard to these    *
- * programs or to the documentation contained in these books. The authors *
- * and publisher shall not be liable in any event for incidental or       *
- * consequential damages in connection with, or arising out of, the       *
- * furnishing, performance, or use of these programs.                     *
- *************************************************************************/
-
- 
+} 
