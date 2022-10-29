b43508c9529d3b4fb2ee8254ab8d265c6d293468
Estilotabi <115471921+taironeAlves@users.noreply.github.com>
2022-10-29 17:45:59 -0300

    update_1745

--- capitulo3/Account.java
+++ capitulo3/Account.java
@@ -1,13 +1,31 @@
 package capitulo3;
 
 public class Account {
-   private String name; // instance variable
+   private String name; 
+   private double balance;
 
    // Metodo contrutor
-   public Account(String name) {
+   public Account(String name, double balance) {
       this.name = name;
+
+      if(balance > 0.0) {
+         this.balance = balance;
+      }
+
    }
 
+
+   // Metodo get deposito
+   public double getBalance() {
+      return balance;
+   }
+
+   public void deposit(double depositAmount) {
+      if(depositAmount > 0.0) {
+         balance += depositAmount;
+      }
+   }
+   
    // method to set the name in the object
    public void setName(String name) {
       this.name = name; // store the name
@@ -17,19 +35,4 @@ public class Account {
    public String getName() {
       return name; // return value of name to caller
    }
-} // end class Account
-
-/**************************************************************************
- * (C) Copyright 1992-2014 by Deitel & Associates, Inc. and *
- * Pearson Education, Inc. All Rights Reserved. *
- * *
- * DISCLAIMER: The authors and publisher of this book have used their *
- * best efforts in preparing the book. These efforts include the *
- * development, research, and testing of the theories and programs *
- * to determine their effectiveness. The authors and publisher make *
- * no warranty of any kind, expressed or implied, with regard to these *
- * programs or to the documentation contained in these books. The authors *
- * and publisher shall not be liable in any event for incidental or *
- * consequential damages in connection with, or arising out of, the *
- * furnishing, performance, or use of these programs. *
- *************************************************************************/
+} 
--- capitulo3/AccountTest.java
+++ capitulo3/AccountTest.java
@@ -2,27 +2,38 @@ package capitulo3;
 
 import java.util.Scanner;
 
-public class AccountTest
-{
-   public static void main(String[] args)
-   { 
+public class AccountTest {
+   public static void main(String[] args) {
+
+      // create an Account object and assign it to myAccount
+      Account account1 = new Account("Tairone Alves Barreto", 50.0);
+      Account account2 = new Account("Desenvolverdor JAVA MAN!", -10.0);
+
+      // Exibo o saldo inicial de cada conta
+      System.out.printf("%s balance: $%.2f %n", account1.getName(), account1.getBalance());
+      System.out.printf("%s balance: $%.2f %n%n", account2.getName(), account2.getBalance());
+
       // create a Scanner object to obtain input from the command window
       Scanner input = new Scanner(System.in);
 
-      // create an Account object and assign it to myAccount
-      Account myAccount = new Account(); 
+      System.out.print("Enter deposit amount for acoount1: ");
+      double depositAmount = input.nextDouble();
+      System.out.printf("%nadding %.2f to account1 balance %n%n", depositAmount);
+      account1.deposit(depositAmount);
 
-      // display initial value of name (null)
-      System.out.printf("Initial name is: %s%n%n", myAccount.getName());
+      // Exibe o saldo
+      System.out.printf("%s balance: $%.2f %n", account1.getName(), account1.getBalance());
+      System.out.printf("%s balance: $%.2f %n", account2.getName(), account2.getBalance());
 
-      // prompt for and read name
-      System.out.println("Please enter the name:");
-      String theName = input.nextLine(); // read a line of text
-      myAccount.setName(theName); // put theName in myAccount
-      System.out.println(); // outputs a blank line
+      // Account 2
+      System.out.print("Enter deposit amount for acoount2: ");
+      depositAmount = input.nextDouble();
+      System.out.printf("%nadding %.2f to account2 balance %n%n", depositAmount);
+      account2.deposit(depositAmount);
 
-      // display the name stored in object myAccount
-      System.out.printf("Name in object myAccount is:%n%s%n",
-         myAccount.getName());
-   } 
-} 
+      // Exibe o saldo
+      System.out.printf("%s balance: $%.2f %n", account1.getName(), account1.getBalance());
+      System.out.printf("%s balance: $%.2f %n", account2.getName(), account2.getBalance());
+
+   }
+}
